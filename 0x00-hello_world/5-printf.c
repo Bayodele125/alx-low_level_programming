a443a50497544a8a9f7d5fa8fc7ae1a8c3148cd3
Bayodele125 <109804362+Bayodele125@users.noreply.github.com>
2022-12-08 16:33:41 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - function for printing without the printf function
+* Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
