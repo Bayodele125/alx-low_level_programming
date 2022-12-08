f5945c9b936d0d2f4ffb409eeba82a6c2404c4b2
Bayodele125 <109804362+Bayodele125@users.noreply.github.com>
2022-12-08 17:40:00 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
 *main - main function
-*Return: return 1 
+*Return: return 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
