70ae56e6047fc15767843ea1f557bb37991b160b
Bayodele <oguntomiloyebayodele@gmail.com>
2022-12-08 07:06:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>  
 #include <string.h>
-/* function for printing without the printf function */
-int main(void) {
+/** 
+ * function for printing without the printf function 
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 } 
