0fd406c537fe1ad7e1d25fd8d97f1c505ed9f17f
Bayodele125 <109804362+Bayodele125@users.noreply.github.com>
2022-12-08 17:36:11 +0100

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*main - main function
+*Return: return 1 
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (1);
+}
