661469fb2cf7189a6b32d95715f462316673338c
Bayodele125 <109804362+Bayodele125@users.noreply.github.com>
2022-12-08 16:28:59 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>  
 #include <string.h>
-/** 
- * main - function for printing without the printf function 
+/**
+* main - function for printing without the printf function
+* Return: 1 if the number is positive. 0 otherwise
  */
 int main (void)
 {
-	puts(" \"Programming is like building a multilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0; 
 }
