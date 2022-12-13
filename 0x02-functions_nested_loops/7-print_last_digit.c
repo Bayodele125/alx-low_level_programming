3a6c9c978650c0c7b52bd87ffa71b318ba86888d
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-13 15:02:42 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-islower.c
+ * Auth: BAYODELE FAITH B.O
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number..
+ *@n: The character to be checked.
+ *
+ * Return: last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
