2b9245f487fd6531fcc690c331b4b2c8397d6002
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-13 16:21:15 +0100

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,8 +13,8 @@ void print_times_table(int n)
 {
         int num, mult, prod;
 
-	if ((n !> 15) || (n !< 0))
-
+	if (n >= 0 && n <= 15))
+	{
         	for (num = 0; num <= n; num++)
         	{
                 	_putchar('0');
@@ -35,5 +35,6 @@ void print_times_table(int n)
                 	}
                 	_putchar('\n');
         	}
+	}
 }
 
