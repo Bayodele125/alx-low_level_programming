dd5d5faf781a9dceb579995462e5dea081a2df4f
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-13 15:34:41 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+/*
+*File: 10-add.c
+*
+*AUTH: BAYODELE FAITH B.O
+*/
+
+#include "main.h"
+
+/**
+*add - adds two integers and returns the result.
+*
+*@a: fnst integer
+*
+*@b: second integer
+*Retuns: sum of two intergers
+*/
+
+int add(int a, int b)
+{
+	int sum = a+b;
+
+	return (sum);
+}
