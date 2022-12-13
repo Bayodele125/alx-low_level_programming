20a9a586b5b2af8cc6188fc27c1eb9a47bb934c8
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-13 15:56:48 +0100

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+/*
+*File: 100-times_table
+*AUth: BAYODELE FAITH B.O
+*/
+
+#include "main.h"
+
+/**
+*times_table -  prints the n times table, starting with 0.
+*/
+
+int print_times_table(int n)
+{
+        int num, mult, prod;
+
+	if ((n > 15) || (n <0)
+		break;
+
+        for (num = 0; num <= n; num++)
+        {
+                _putchar('0');
+
+                for (mult = 1; mult <= n; mult++)
+                {
+                        _putchar(',');
+                        _putchar(' ');
+
+                        prod = num * mult;
+
+                        if (prod <= n)
+                                _putchar(' ');
+                        else
+                                _putchar((prod / 10) + '0');
+
+                        _putchar((prod % 10) + '0');
+                }
+                _putchar('\n');
+        }
+}
+
