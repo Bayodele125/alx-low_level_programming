ab856f0d1b20dfc85ebad41fc5398562b9833f52
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-13 14:52:35 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+/*
+ * File: 3-islower.c
+ * Auth: BAYODELE FAITH B.O
+ */
+
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of integer.
+ *@n: The character to be checked.
+ *
+ * Return: positive value of n
+ */
+int -abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
