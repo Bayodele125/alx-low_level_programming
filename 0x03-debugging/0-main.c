c2551f1bd75ecfbce4bd53a05229c31b2068d63c
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-15 03:39:38 +0100

    rm positive_or_negative.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,12 @@
-/*
-*File: 0-main.c
-*
-*AUTH: Bayodele Faith B.O
-*/
-
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for positive or negative.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-        positive_or_negative(int 0);
+        positive_or_negative(0);
         return (0);
 }
