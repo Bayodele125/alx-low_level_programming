6655ba9304dc2d04298acccadf1cee0a8f339399
bayodele <oguntomiloyebayodele@gmail.com>
2022-12-15 03:11:16 +0100

    1-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+/*
+*File: 0-main.c
+*
+*AUTH: Bayodele Faith B.O
+*/
+
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        positive_or_negative(int 0);
+        return (0);
+}
