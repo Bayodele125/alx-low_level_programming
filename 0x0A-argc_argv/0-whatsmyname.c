c8a467e5523d7310a87eec6ef9e0e30b81636b20
bayodele <oguntomiloyebayodele@gmail.com>
2023-01-11 10:24:04 +0100

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
-* main - prints its name, followed by a new line.
-* @argc: The lenght of the command line arg
-*@argv: Array of argc
-* Return: Always (0)
-*/
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: Array of argc
+ * Return: Always (0)
+ */
 
 int main(int argc, char* argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
